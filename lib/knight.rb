9a365f39ca21db3607d0c7616e912e6be348b443
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-04-12 18:39:38 -0700

    Rework the all_paths method
    Reworked the all_paths method and fixed issues in the method.
    Starting to add limits in order to prevent system from hanging
    from creating too many Knight objects in the possible_paths
    variable.

--- lib/knight.rb
+++ lib/knight.rb
@@ -13,56 +13,58 @@ def print_board(board)
     end
 end
 
-board = create_board
-print_board(board)
-
 #lists all possible paths that will lead to our ending goal
 #generates all moves possible from our staring spot
 #goes through and checks if each move is out-of-bounds or has already been played
 #generates a new node in our all_paths tree
 #stops when no spots are available to travel to or when the goal is reached
 
-def all_paths(start, goal, board = create_board, history = [])
-    if start == goal
+def all_paths(start, goal, history = [])
+    if start == goal || history.length >= 10
         return
     end
-    current_point = Move.new(start, goal, board, history)
-    x_coord = start[0]
-    y_coord = start[1]
+    temp_history = []
+    history.each do |choice|
+        temp_history.push(choice)
+    end
+    current_point = Knight.new(start, goal, temp_history)
+    current_point.history.push(start)
     legal_moves = []
-    possible_x = [-2, -2, -1, -1, 1, 1, 2, 2].map {|element| element + x_coord}
-    possible_y = [-1, 1, -2, 2, -2, 2, -1, 1].map {|element| element + y_coord}
+    possible_x = [-2, -2, -1, -1, 1, 1, 2, 2].map {|element| element + start[0]}
+    possible_y = [-1, 1, -2, 2, -2, 2, -1, 1].map {|element| element + start[1]}
     (0..7).each do |index|
         if ((possible_x[index] in (0..7)) && (possible_y[index] in (0..7)))
             legal_moves.push([possible_x[index], possible_y[index]])
         end
     end
-    legal_moves.keep_if {|spot| (board[spot[0]][spot[1]] != 'P') && !(history.include?(spot))}
+    legal_moves.keep_if {|spot| !(temp_history.include?(spot))}
     return if legal_moves.empty?
     legal_moves.each do |move|
-        board[move[0]][move[1]] = 'P'
-        history.push(move)
-        path = self.all_paths(move, goal, board, history)
-        current_point.history.push(history)
+        path = self.all_paths(move, goal, temp_history)
         current_point.possible_paths.push(path)
     end
     return current_point
 end
 
-class Move
+def find_shortest_path(goal, tree)
+    return tree.history if tree.start == goal
+    tree.possible_paths.each {|path| self.find_shortest_path(goal, path)}
+end
+
+class Knight
     attr_accessor :start, :goal, :history, :possible_paths
 
-    def initialize(start, goal, board = create_board, history = [])
+    def initialize(start, goal, history = [])
         @start = start
         @goal = goal
-        @board = board
         @history = history
         @possible_paths = []
     end
 end
 
+board = create_board
 choice = all_paths([0,0], [2,4])
 puts choice.possible_paths
-print choice.history
-puts choice.possible_paths.each {|path| puts path}
+#print choice.history
+#puts choice.possible_paths.each {|path| puts path}
 
